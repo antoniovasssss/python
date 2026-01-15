756077cd7fbf9bfbced3ee59ca6a7d56f7be35de
antoniovasssss <antoniovass087@gmail.com>
2026-01-15 10:00:33 +0000

    First commit

--- a_functions/snippet_0_2.py
+++ a_functions/snippet_0_2.py
@@ -1,13 +1,14 @@
 def how_many():
     return 42
-
+# this prints the function object itself, not the result of calling it
 print(how_many)
+# the parenthesis () call the function, which returns 42, so this prints 42
 print(how_many())
-
+# since the_answer was assigned the return value of how_many(), this also prints 42
 the_answer = how_many()
 print(the_answer)
 
 def how_many():
-    5 #does nothing
+    5 # does nothing
 
 print(how_many())
